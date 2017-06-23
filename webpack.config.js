b6b15999454d4ff12bdb59604bcae3acf52fe8cf
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2017-06-23 11:33:53 +0200

    init

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.less$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'less-loader',
+        ]
+      }
+    ]
+  },
+  devServer: {
+    contentBase: './dist/',
+    inline: true
+  }
+};
